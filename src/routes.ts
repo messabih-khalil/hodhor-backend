5019013035f91a20fb9adf3a0c4bd50b3052c5ab
messabih <msbih.khalil@gmail.com>
2023-09-01 05:26:51 +0100

    feat(routes.ts): add universityRouter to handle routes for '/api/v1/univ' endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
+import { universityRouter } from '@api/university/univ.routes';
 import { Application } from 'express';
+
+const BASE_URL = '/api/v1';
 
 export default (app: Application) => {
     const routes = () => {
-        console.log(app);
+        app.use(`${BASE_URL}/univ`, universityRouter.routes());
     };
 
     routes();
