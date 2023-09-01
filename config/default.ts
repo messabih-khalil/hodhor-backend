9bf6a54ada4c0306f77ed3e4f76e4cedd000437f
messabih <msbih.khalil@gmail.com>
2023-09-01 05:25:31 +0100

    chore(default.ts): rename redis_client variable to redis_host for clarity and consistency with the actual purpose of the variable

--- config/default.ts
+++ config/default.ts
@@ -2,7 +2,7 @@ export default {
     server_port: process.env.SERVER_PORT || 5000,
     node_env: process.env.NODE_ENV,
     database_uri: process.env.DATABASE_URI,
-    redis_client: process.env.REDIS_CLIENT,
+    redis_host: process.env.REDIS_HOST,
     jwt_secret_key: process.env.JWT_SECRET_KEY,
     secret_key_one: process.env.SECRET_KEY_ONE,
     secret_key_two: process.env.SECRET_KEY_TWO,
