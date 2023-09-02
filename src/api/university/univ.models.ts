86598b4c08802f28502b82e772a61a271217aa7d
messabih <msbih.khalil@gmail.com>
2023-09-02 21:17:13 +0100

    feat(univ.models.ts): add University model with password encryption and comparison methods
    The University model is added to handle university data in the application. It includes fields for name, location, root, and password. The password field is encrypted using bcrypt before saving to the database.
    
    The model includes the following methods:
    - `encryptPassword(password: string): Promise<string>`: A static method that takes a password as input and returns the hashed password.
    - `comparePassword(candidatePassword: string): Promise<boolean>`: An instance method that takes a candidate password as input and compares it with the stored hashed password.
    
    These methods provide functionality for encrypting and comparing passwords for the University model.

---
+++ src/api/university/univ.models.ts
@@ -0,0 +1,61 @@
+import mongoose, { Schema, Model } from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+import { IUniversity } from '@university/index';
+
+// Interface for the University model
+interface IUniversityModel extends Model<IUniversity> {
+    encryptPassword(password: string): Promise<string>;
+    comparePassword(candidatePassword: string): Promise<boolean>; 
+}
+
+const universitySchema = new Schema<IUniversity>(
+    {
+        name: { type: String, required: true, index: true, unique: true },
+        location: { type: String, required: true },
+        root: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+    },
+    { timestamps: true }
+);
+
+// Hash the password before saving
+universitySchema.pre<IUniversity>('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(this.password, salt);
+        this.password = hashedPassword;
+        next();
+    } catch (error: any) {
+        return next(error);
+    }
+});
+
+// Static method to encrypt a password
+universitySchema.statics.encryptPassword = async function (
+    password: string
+): Promise<string> {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return hashedPassword;
+};
+
+// Instance method to compare plain text password with stored hashed password
+universitySchema.methods.comparePassword = async function (
+    candidatePassword: string
+): Promise<boolean> {
+    try {
+        return await bcrypt.compare(candidatePassword, this.password);
+    } catch (error) {
+        throw error;
+    }
+};
+
+const University: IUniversityModel = mongoose.model<
+    IUniversity,
+    IUniversityModel
+>('University', universitySchema);
+
+export default University;
