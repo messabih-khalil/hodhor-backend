ff619003315e21cef9886d7802b6f56794d163cc
messabih <msbih.khalil@gmail.com>
2023-09-02 21:17:46 +0100

    feat(univ.routes.ts): add university routes for creating, logging in, and logging out universities
    This commit adds a new file `univ.routes.ts` which contains the routes for universities. The routes include a POST route for creating a university, a POST route for logging in a university, and a POST route for logging out a university. The `isAuthenticated` middleware is used to protect the logout route.

---
+++ src/api/university/univ.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import { UniversityHandlers } from '@university/index';
+import { isAuthenticated } from '@middlewares/isAuthenticated.middleware';
+
+class UnivertsityRouter {
+    private router: Router;
+
+    constructor() {
+        this.router = Router();
+    }
+
+    routes(): Router {
+        this.router.post('/', UniversityHandlers.prototype.create);
+        this.router.post('/login', UniversityHandlers.prototype.login);
+        this.router.post(
+            '/logout',
+            [isAuthenticated],
+            UniversityHandlers.prototype.logout
+        );
+
+        return this.router;
+    }
+}
+
+export const universityRouter: UnivertsityRouter = new UnivertsityRouter();
