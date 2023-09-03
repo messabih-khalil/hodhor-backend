7baf5db2c37e01570911db80534a99d97ae1ea9c
messabih <msbih.khalil@gmail.com>
2023-09-03 21:39:29 +0100

    feat(department.routes.ts): add department routes for creating, logging in, and logging out departments
    The department.routes.ts file was added to the project. It contains the implementation of the department routes. The routes include:
    - POST /: route for creating a new department, requires authentication
    - POST /login: route for logging in a department
    - POST /logout: route for logging out a department, requires authentication
    
    These routes were added to provide functionality for managing departments in the application.

---
+++ src/api/department/department.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import { isAuthenticated } from '@middlewares/isAuthenticated.middleware';
+import DepartmentHandlers from '@department/department.handlers';
+class DepartmentRouter {
+    private router: Router;
+
+    constructor() {
+        this.router = Router();
+    }
+
+    routes(): Router {
+        this.router.post(
+            '/',
+            [isAuthenticated],
+            DepartmentHandlers.prototype.createDepartment
+        );
+        this.router.post('/login', DepartmentHandlers.prototype.login);
+        this.router.post(
+            '/logout',
+            [isAuthenticated],
+            DepartmentHandlers.prototype.logout
+        );
+
+        return this.router;
+    }
+}
+
+export const departmentRouter: DepartmentRouter = new DepartmentRouter();
