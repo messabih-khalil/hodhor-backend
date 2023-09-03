b4436c66f04dad998007366750e4fd49c10130eb
messabih <msbih.khalil@gmail.com>
2023-09-03 21:38:30 +0100

    feat(department.cache.ts): add DepartmentCache class to handle caching of department data
    The DepartmentCache class is responsible for caching department data. It extends the BaseCache class from the redis.base module. The constructor initializes the cache with the name 'departmentCache'.
    
    The class provides a method addDepartmentToCache() to add department data to the cache. If the cache client is not open, it connects to the client. Then, it uses the HSET command to set the department data in the cache using the departmentKey() function from the redis.keys module.
    
    The class also provides two helper methods, serializeData() and deserializeData(), to serialize and deserialize the department data respectively.
    
    This commit adds the DepartmentCache class to handle caching of department data.

---
+++ src/api/department/department.cache.ts
@@ -0,0 +1,35 @@
+import { BaseCache } from '@helpers/redis/redis.base';
+import { DepartmentData } from './department.types';
+import { departmentKey } from '@helpers/redis/redis.keys';
+
+class DepartmentCache extends BaseCache {
+    constructor() {
+        super('departmentCache');
+    }
+
+    // add department
+
+    async addDepartmentToCache(departmentData: DepartmentData) {
+        if (!this.client.isOpen) {
+            await this.client.connect();
+        }
+
+        await this.client.HSET(
+            departmentKey(departmentData.email),
+            'data',
+            this.serializeData(departmentData)
+        );
+    }
+
+    serializeData(departmentData: DepartmentData) {
+        return JSON.stringify({
+            ...departmentData,
+        });
+    }
+
+    deserializeData(departmentData: string) {
+        return JSON.parse(departmentData);
+    }
+}
+
+export const departmentCache: DepartmentCache = new DepartmentCache();
