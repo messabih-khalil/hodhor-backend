195d4115ee67863ce533af7c15f2eb1d2ed382bf
messabih <msbih.khalil@gmail.com>
2023-09-13 15:39:28 +0100

    feat(config): add support for environment variables SENDGRID_API_KEY, SENDER_EMAIL, and SENDER_EMAIL_PASSWORD to configure SendGrid API key and sender email credentials

--- config/default.ts
+++ config/default.ts
@@ -1,9 +1,13 @@
 export default {
     server_port: process.env.SERVER_PORT || 5000,
+    client_url: process.env.CLIENT_URL,
     node_env: process.env.NODE_ENV,
     database_uri: process.env.DATABASE_URI,
     redis_host: process.env.REDIS_HOST,
     jwt_secret_key: process.env.JWT_SECRET_KEY,
     secret_key_one: process.env.SECRET_KEY_ONE,
     secret_key_two: process.env.SECRET_KEY_TWO,
+    sendgrid_api_key: process.env.SENDGRID_API_KEY,
+    sender_email: process.env.SENDER_EMAIL,
+    sender_email_password: process.env.SENDER_EMAIL_PASSWORD,
 };
