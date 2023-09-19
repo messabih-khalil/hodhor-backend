188dd8da9e54a244c434de6c8de8455e7acdefdb
messabih <msbih.khalil@gmail.com>
2023-09-19 23:03:00 +0100

    refactor(routes.ts): remove unused imports and commented out code feat(routes.ts): remove redundant authentication routes and cookie test route

--- src/routes.ts
+++ src/routes.ts
@@ -4,8 +4,8 @@ import { groupRouter } from '@api/group/groups.routes';
 import { studentRouter } from '@api/students/student.routes';
 import { teacherRouter } from '@api/teachers/teacher.routes';
 import { universityRouter } from '@api/university/univ.routes';
-import { Application, Request, Response, NextFunction } from 'express';
-import { NotAuthorizedError } from '@utils/error-handlers';
+import { Application } from 'express';
+
 import { authRouter } from '@api/auth/auth.routes';
 
 const BASE_URL = '/api/v1';
@@ -18,31 +18,6 @@ export default (app: Application) => {
         app.use(`${BASE_URL}/students`, studentRouter.routes());
         app.use(`${BASE_URL}/teachers`, teacherRouter.routes());
         app.use(`${BASE_URL}/auth`, authRouter.routes());
-
-        // TODO move this to auth routes
-        app.get(
-            `${BASE_URL}/auth/me`,
-            (req: Request, res: Response, next: NextFunction) => {
-                if (req.session?.role) {
-                    return res.status(HTTP_STATUS.OK).json({
-                        status: 'success',
-                        role: req.session?.role,
-                    });
-                }
-
-                return next(
-                    new NotAuthorizedError(
-                        'Your Not Authorized to perform this action'
-                    )
-                );
-            }
-        );
-
-        app.get(`${BASE_URL}/test/cookie`, (req, res) => {
-            res.status(200).json({
-                cookie: req.session,
-            });
-        });
     };
 
     routes();
